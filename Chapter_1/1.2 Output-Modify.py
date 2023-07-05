8222cd789fba8276ea2a96ccbce51e5e8a58cff2
shiüo <shiueo.csh@gmail.com>
2023-07-05 18:16:22 -0500

    awef

---
+++ Chapter_1/1.2 Output-Modify.py
@@ -0,0 +1,27 @@
+print("Python supports UTF-8! 와우!") # Modified message
+
+print("------------------------------------") # Seperator
+
+# We can add lines by using '\n' or by using print statement with a blank
+print("What do you call a factory that makes okay products?")
+print("") # or print()
+print("") # or print()
+print("A satisfactory.")
+
+print("------------------------------------") # Seperator
+
+# OR
+print("What do you call a factory that makes okay products? \n\n\nA satisfactory.")
+
+
+print("------------------------------------") # Seperator
+# ¯\_(ツ)_/¯
+print("What do you call a factory that makes okay products?")
+for i in range(2):
+  print('\n')
+print("A satisfactory.")
+
+print("------------------------------------") # Seperator
+# I remember that there were \t \r \' \" things..
+
+print('tab test\t hello') # tab
