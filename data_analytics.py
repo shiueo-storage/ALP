4d4afd0dcf6e970f5f1efcec23023f863ecd60a6
shiüo <shiueo.csh@gmail.com>
2023-07-11 00:14:18 -0500

    e

--- data_analytics.py
+++ data_analytics.py
@@ -1,2 +1,37 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
+df = pd.read_csv("./Austin_Animal_Center_Intakes.csv")
+
+ages = df['Age upon Intake']
+
+time_dict = {}
+
+for i in ages:
+    n, times = map(str, i.split())
+    n = int(n)
+    if 'day' in times:
+        pass
+    elif 'week' in times:
+        n *= 7
+    elif 'month' in times:
+        n *= 30
+    elif 'year' in times:
+        n *= 365
+
+    if n > 0:
+        if n not in time_dict:
+            time_dict[n] = 1
+        else:
+            time_dict[n] += 1
+
+x_axis = sorted(time_dict.keys())
+print(x_axis)
+
+y_axis = []
+for i in x_axis:
+    y_axis.append((time_dict[i] / len(x_axis)) * 100)
+
+x_axis = [str(i) + " days" for i in x_axis]
+plt.pie(y_axis, labels=x_axis, autopct='%.1f%%', startangle=0, counterclock=False)
+plt.show()
---
+++ test.py
@@ -0,0 +1,2 @@
+with open("abc.txt", 'r') as f:
+    print(f.read())
